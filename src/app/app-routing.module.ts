0ee9c6f41d367f37e93e92d52ae5fd7511222390
gsspatil31 <shinde.patilgs1214@gmail.com>
2023-01-15 17:44:25 +0530

    save the changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FeatureComponent } from './feature/feature.component';
+import { HomeComponent } from './home/home.component';
+import { MeetComponent } from './meet/meet.component';
+import { ServiceComponent } from './service/service.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",
+    component:HomeComponent
+  },
+  {
+    path:"feature",
+    component:FeatureComponent
+  },
+  {
+    path:"service",
+    component:ServiceComponent
+  },
+  {
+    path:"testimonial",
+    component:TestimonialComponent
+  },
+  {
+    path:"meet",
+    component:MeetComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+import { FeatureComponent } from './feature/feature.component';
+import { ServiceComponent } from './service/service.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
+import { MeetComponent } from './meet/meet.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FeatureComponent,
+    ServiceComponent,
+    TestimonialComponent,
+    MeetComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
