afee54c962203da20b7f57ecd14cc6f383ce3c7d
gsspatil31 <shinde.patilgs1214@gmail.com>
2023-01-19 21:08:24 +0530

    removed unwanted codes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,32 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FeatureComponent } from './web-blog/components/feature/feature.component';
-import { HomeComponent } from './web-blog/components/home/home.component';
-import { MeetComponent } from './web-blog/components/meet/meet.component';
-import { ServiceComponent } from './web-blog/components/service/service.component';
-import { TestimonialComponent } from './web-blog/components/testimonial/testimonial.component';
 
 const routes: Routes = [
   {
-    path:"",
-    component:HomeComponent
-  },
-  {
-    path:"feature",
-    component:FeatureComponent
-  },
-  {
-    path:"service",
-    component:ServiceComponent
-  },
-  {
-    path:"testimonial",
-    component:TestimonialComponent
-  },
-  {
-    path:"meet",
-    component:MeetComponent
-  },
+    
+    path: 'feature',
+    loadChildren: () => import('./feature/feature.module').then(m => m.FeatureModule)
+
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HomeComponent } from './web-blog/components/home/home.component';
-import { FeatureComponent } from './web-blog/components/feature/feature.component';
-import { ServiceComponent } from './web-blog/components/service/service.component';
-import { TestimonialComponent } from './web-blog/components/testimonial/testimonial.component';
-import { MeetComponent } from './web-blog/components/meet/meet.component';
-import { LoginComponent } from './web-blog/components/login/login.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    FeatureComponent,
-    ServiceComponent,
-    TestimonialComponent,
-    MeetComponent,
-    LoginComponent
   ],
   imports: [
     BrowserModule,
