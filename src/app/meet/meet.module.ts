fc69d0f0f0d8e3853bdbb814f2ddad0993df8690
gsspatil31 <shinde.patilgs1214@gmail.com>
2023-01-19 10:25:17 +0530

    create modules for lazyloading

---
+++ src/app/meet/meet.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MeetRoutingModule } from './meet-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MeetRoutingModule
+  ]
+})
+export class MeetModule { }
