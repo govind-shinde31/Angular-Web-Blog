d154106790737657d4212fed90ef1e8bcdc5fa70
gsspatil31 <shinde.patilgs1214@gmail.com>
2023-01-20 13:01:05 +0530

    save changes

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    NgbModule,
+    FormsModule
   ]
 })
 export class LoginModule { }
