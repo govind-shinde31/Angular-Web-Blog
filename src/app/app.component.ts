1bb1fed5f4ee49bcaf6edef1de3e4505875c7fdb
gsspatil31 <shinde.patilgs1214@gmail.com>
2023-01-20 20:58:52 +0530

    added login popup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Angular_WebBlogapp';
+
+  constructor(private modalService:NgbModal){}
+  loginModal() {
+		const modalRef = this.modalService.open(LoginComponent);
+		
+	}
 }
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login.component';
 
-import { LoginRoutingModule } from './login-routing.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent],
+  exports:[LoginComponent],
   imports: [
     CommonModule,
-    LoginRoutingModule
   ]
 })
 export class LoginModule { }
